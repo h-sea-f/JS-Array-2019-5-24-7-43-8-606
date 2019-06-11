0d0f8289c960b4e4abdc4396730ddb69a6a6dd19
h-sea-f <h_sea_f@163.com>
2019-06-11 23:21:22 +0800

    6.11

--- index.js
+++ index.js
@@ -4,25 +4,47 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
-
+console.log(Array.isArray(a));
+console.log(Array.isArray(b));
 
 // 编写程序，将下面数组中的每一项都乘以2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
-
+for(var i=0;i<a.length;i++){
+	a[i]=a[i]*2;
+}
 
 // 编写程序，按下面的要求输出结果。
 var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
-
+console.log(colors.join(" "));
+console.log(colors.join("+"));
+console.log(colors.join(","));
 
 // 编写程序，将下面数组中的数字按从大到小的顺序排序。
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+function sortNumber(a,b)
+{
+	return a - b
+}
 
+console.log(a.sort(sortNumber));
 
 // 编程程序，找出下列数组中出现频率最高的元素。
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+var h={};
+var maxNum=0;
+var maxEle=null;
+for(var i=0;i<a.length;i++){
+	var b=a[i];
+	h[b]===undefined? h[b]=1:(h[b]++);
+	if(h[b]>maxNum){
+		maxEle=b;
+		maxNum=h[b];
+	}
+}
+console.log(maxEle);
